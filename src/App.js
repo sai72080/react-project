c46734cd97e09b1aa3a688ff9d8c1dfb36c36cbd
sai72080 <t>
2023-07-28 13:36:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+import { ToastContainer } from 'react-toastify';
+import Appheader from './Appheader';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+        <Appheader />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer theme='colored' position='top-center'></ToastContainer>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="row-container">
+      <h1 className="text-center">Welcome to Tata-cliq app</h1>
+      India, officially the Republic of India (ISO: Bhārat Gaṇarājya),[25] is a country in South Asia. It is the seventh-largest country by area; the most populous country as of June 2023;[26][27] and from the time of its independence in 1947, the world's most populous democracy.[28][29][30] Bounded by the Indian Ocean on the south, the Arabian Sea on the southwest, and the Bay of Bengal on the southeast, it shares land borders with Pakistan to the west;[j] China, Nepal, and Bhutan to the north; and Bangladesh and Myanmar to the east. In the Indian Ocean, India is in the vicinity of Sri Lanka and the Maldives; its Andaman and Nicobar Islands share a maritime border with Thailand, Myanmar, and Indonesia.
+      <div className="text-center">
+      </div>
+    </div>
+  );
+};
+
+export default Home;
